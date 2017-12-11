1c2eb7525cb2026000f2a921e077855ad8829166
Prabhakar Vatsa <prabhakarvatsa@gmail.com>
2017-12-11 22:06:41 +0530

    Joe Rock ListView Tutorials till yt vid 5.

---
+++ App1/App1/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private List<Person> mItems;
+        private ListView mListView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+            mItems = new List<Person>();
+            mItems.Add(new Person { FirstName = "Ramesh", LastName = "Kumar", Age = "22", Gender = "Male" });
+            mItems.Add(new Person { FirstName = "Suresh", LastName = "Dixit", Age = "35", Gender = "Male" });
+            mItems.Add(new Person { FirstName = "Priya", LastName = "Kumari", Age = "30", Gender = "Female" });
+
+            //From Tutorial 2
+            //ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+
+            //From Tutorial 3 onwards till
+            MyListViewAdapter adapter = new MyListViewAdapter(this, mItems);
+
+            mListView.Adapter = adapter;
+            mListView.ItemClick += MListView_ItemClick;
+            mListView.ItemLongClick += MListView_ItemLongClick;
+        }
+
+        private void MListView_ItemLongClick(object sender, AdapterView.ItemLongClickEventArgs e)
+        {
+            Console.WriteLine(mItems[e.Position].LastName);
+        }
+
+        private void MListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Console.WriteLine(mItems[e.Position].FirstName);
+        }
+    }
+}
+
---
+++ App1/App1/MyListViewAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    class MyListViewAdapter : BaseAdapter<Person>
+    {
+        private List<Person> mItems;
+        private Context mContext;
+
+        public MyListViewAdapter(Context context, List<Person> items)
+        {
+            mItems = items;
+            mContext = context;
+        }
+
+        public override Person this[int position] => mItems[position];
+
+        public override int Count => mItems.Count;
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+
+            if (row == null)
+            {
+                row = LayoutInflater.From(mContext).Inflate(Resource.Layout.listview_row, null, false);
+            }
+
+            TextView txtFirstName = row.FindViewById<TextView>(Resource.Id.txtFirstName);
+            txtFirstName.Text = mItems[position].FirstName;
+
+            TextView txtLastName = row.FindViewById<TextView>(Resource.Id.txtLastName);
+            txtLastName.Text = mItems[position].LastName;
+
+            TextView txtAge = row.FindViewById<TextView>(Resource.Id.txtAge);
+            txtAge.Text = mItems[position].Age;
+
+            TextView txtGender = row.FindViewById<TextView>(Resource.Id.txtGender);
+            txtGender.Text = mItems[position].Gender;
+
+            return row;
+        }
+    }
+}
